c70ee4a0edbec9018bc53b43b09e2e4a18a180a2
Andrei <andsemenov@gmail.com>
2020-10-10 13:51:23 +0100

    Server part for challenge

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3000;
+const stratford = require("./data/Stratford.json");
+const heathrow = require("./data/Heathrow.json");
+//const harrow = require("./data/Harrow.json");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/:city/:category", (request, response) => {
+  const city = request.params.city;
+  const category = request.params.category;
+  const allGuidesInCity = require(`./data/${capitalize(city)}.json`);
+
+  const searchedGuide = allGuidesInCity[category];
+  response.json(searchedGuide);
+});
+
+function capitalize(str) {
+  return str && str[0].toUpperCase() + str.slice(1);
+}
+
+app.listen(port, () => console.log(`Server is listennig on port ${port}!`));
